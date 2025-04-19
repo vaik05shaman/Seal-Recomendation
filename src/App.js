8f903453e92d248cce0dd8a080b1e1374d378722
vaik05shaman <vaibhav10082005@gmail.com>
2025-04-19 23:28:36 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import DashBord from "./pages/DashBord";
-
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
-    <DashBord/>
+   
+    <Header />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Footer />
     </div>
   );
 }
